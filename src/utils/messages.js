122600aacc48d4c6cfd08e51487c208e43e04516
vinay <vinay.kgupta7@gmail.com>
2020-10-16 18:51:11 +0800

    file added

---
+++ src/utils/messages.js
@@ -0,0 +1,14 @@
+const generateMessage = (username, text) => {
+    return {
+        username,
+        text,
+        createdAt: new Date().getTime()
+    }
+}
+
+
+
+module.exports = {
+    generateMessage
+    
+}
